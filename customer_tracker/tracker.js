175b423375468a3095ba4088ea2c8f7618cdb4c6
Mumin8 <alhassanmumin8@gmail.com>
2024-01-21 23:42:19 +0000

    refactor

--- customer_tracker/tracker.js
+++ customer_tracker/tracker.js
@@ -1,8 +1,11 @@
+// this variables get the html elements through dom
 const input_elem = document.getElementById("input-id");
 const display_elem = document.getElementById("display-item");
 const save_elem = document.getElementById('save')
 const delete_elem = document.getElementById("delete")
 
+
+// condition to check for availability of items in local storage
 if (localStorage.names) {
     listItems = JSON.parse(localStorage.names);
     display(listItems)
@@ -11,7 +14,7 @@ if (localStorage.names) {
 }
 
 
-// this will save a url from an input
+// this will save a url from an input when a button is clicked
 save_elem.addEventListener("click", function(){
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let url_value = tabs[0].url;
@@ -27,13 +30,15 @@ save_elem.addEventListener("click", function(){
 })
 
 
-// this will delete the urls
+// this will delete the urls when a button is clicked
 delete_elem.addEventListener("click", function(){
     listItems = []
     clear_fromLS()
     clear_page()
 })
 
+
+// this function displays the url on the page
 function display(items){
     clear_page()
     for (let i = 0; i < items.length; i++) {
@@ -49,22 +54,30 @@ function display(items){
 
         li.style.listStyle = 'none'
         li.style.padding = '10px'
+        li.style.fontWeight = 'bold'
+
         display_elem.appendChild(li);
     }
 }
 
+
+// this function clears the urls from the page
 function clear_page(){
     display_elem.innerHTML = " "
 }
 
+// this function saves the url into local storage
 function save_toLS(){
     localStorage.setItem("names", JSON.stringify(listItems));
 }
 
+// this will clear the local storage
 function clear_fromLS(){
     localStorage.clear()
 
 }
+
+// This will clear the input after items added to listItems 
  function clear_input(){
     input_elem.value = ""
  }
